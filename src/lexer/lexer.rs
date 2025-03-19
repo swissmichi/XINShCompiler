f668aa9aec41ea75f06d22531e2ee6968a917e44
swissmichi <khrstepan@gmail.com>
2025-03-19 08:03:56 +0100

    Finished the lexer, test it

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -3,90 +3,100 @@ pub mod lexer{
     use lexer::tokens;
     use std::fs;
     use regex::Regex;
+    use std::collections::HashSet;
     // Function takes file adress as input and returns a vector of tokens. 
     pub fn lex(file_path: &str) -> Vec![tokens::Token] {
         let mut cursor: usize = 0;
         let mut tokens_vec: Vec<tokens::Token> = Vec::new();
-        let re_comment = Regex::new(r"^(#.*\n)").unwrap();
+        let keywords: HashSet<&str> = ["num", "float", "text", "bool", "list", "range", "func", "param", "return",
+        "echo", "read", "num2float", "num2text", "num2bool", "num2list", "num2range", "float2num", "float2text", "float2list",
+        "text2num", "text2float", "text2bool", "text2list", "bool2num", "bool2text", "bool2list", "if", "then",
+        "else", "for", "while", "do", "done", "in", "dir", "file", "cat", "cp", "mv", "command"]
+            .iter().cloned().collect();
+
+        let re_comment = Regex::new(r"^#.*\n").unwrap();
+        let re_whitespace = Regex::new(r"\s+").unwrap();
         let re_delim = Regex::new(r#"^("|\[{1,2}|\]{1,2}|\(|\)|\{|\}|,|:|;)"#).unwrap();
         let re_operator = Regex::new(r"^(=|\+|\-|/|\*|<|>|\$)").unwrap();
         
         let re_num_lit = Regex::new(r"^([0-9]+)").unwrap();
-        let re_float_lit = Regex::new(r"^([0-9]+.[0-9]+)").unwrap();
-        let re_string_lit = Regex::new(r#"^"(.*)""#).unwrap();
+        let re_float_lit = Regex::new(r"^([0-9]+\.[0-9]+)").unwrap();
+        let re_string_lit = Regex::new(r#"^(['"])(.*?)\1"#).unwrap();
         let re_bool_lit = Regex::new(r"^(True|False)").unwrap();
 
-        let keyword_identifier = Regex::new(r"^([^\W0-9]*)").unwrap();
+        let re_keyword_identifier = Regex::new(r"^([a-zA-Z_][a-zA-Z0-9_]*)").unwrap();
         
         let source = fs::read_to_string(file_path)
             .expect("Should have been able to read file");
         
         while cursor < source.len() {
-            if re_comment.is_match(source[cursor..]){
-                cursor += re_comment.captures(source[cursor..])
-                    .unwrap()
-                    .get(0)
-                    .unwrap()
-                    .as_str()
-                    .len();
-                continue
-            } else if re_delim.is_match(source[cursor..]){
-                 tokens_vec.push(tokens::Token::Delim(re_delim.captures(source[cursor..])
-                         .unwrap()
-                         .get(1)
-                         .unwrap()
-                         .as_str()));           
-                cursor += re_delim.captures(source[cursor..])
-                    .unwrap()
-                    .get(0)
-                    .unwrap()
-                    .as_str()
-                    .len();
-                continue
-            } else if re_operator.is_match(source[cursor..]){
-                tokens_vec.push(tokens::Token::Operator(re_delim(source[cursor..])
-                        .unwrap()
-                        .get(1)
-                        .unwrap()
-                        .as_str()));
-                cursor += re_operator.captures(source[cursor..])
-                    .unwrap()
-                    .get(0)
-                    .unwrap()
-                    .as_str()
-                    .len();
-                conrinue
-            } else if re_num_lit.is_match(source[cursor..]){
-                    tokens_vec.push(tokens::Token::NumLiteral(re_num_lit.captures(source[cursor..])
-                            .unwrap()
-                            .get(1)
-                            .unwrap()
-                            .as_str()
-                            .parse::<i32>()
-                            .expect("Should have been an integer")));
-                    cursor += re_num_lit.captures(source[cursor..])
-                        .unwrap()
-                        .get(0)
-                        .unwrap()
-                        .as_str()
-                        .len();
-                    continue
-            } else if re_float_lit.is_match(source[cursor..]){
-                tokens_vec.push(tokens::Token::FloatLiteral(re_float_lit.captures(source[cursor..])
-                        .unwrap()
-                        .get(1)
-                        .unwrap()
-                        .as_str()
-                        .parse::<f32>()
-                        .expect("Should have been a float")));
-                cursor += re_float_lit.captures(source[cursor..])
-                    .unwrap()
-                    .get(0)
-                    .unwrap()
-                    .as_str()
-                    .len();
-                continue
-            } // TODO: Continue the pattern with StringLiteral. Test
+            if let Some(token) = re_comment.captures(source[cursor..]) {
+                cursor += token.get(0).unwrap().as_str().len();
+                continue;
+            }
+            if let Some(token) = re_whitespace.captures(source[cursor..]) {
+                cursor += token.get(0).unwrap().as_str().len();
+                continue;
+            }
+            if let Some(token) = re_delim.captures(source[cursor..]) {
+                cursor += token.get(0).unwrap().as_str().len();
+                tokens_vec.push(tokens::Token::Delim(
+                        token.get(1).unwrap().as_str()
+                ));
+                continue;
+            }
+            if let Some(token) = re_operator.captures(source[cursor..]) {
+                cursor += token.get(0).unwrap().as_str().len();
+                tokens_vec.push(tokens::Token::Operator(
+                        token.get(1).unwrap().as_str()
+                ));
+                continue;
+            }
+             if let Some(token) = re_float_lit.captures(source[cursor..]) {
+                cursor += token.get(0).unwrap().as_str().len();
+                tokens_vec.push(tokens::Token::FloatLiteral(
+                        token.get(1).unwrap().as_str()
+                        .parse::<f32>().expect("Should have been a float"),
+                ));
+                continue;
+             } 
+            if let Some(token) = re_num_lit.captures(source[cursor..]) {
+                cursor += token.get(0).unwrap().as_str().len();
+                tokens_vec.push(tokens::Token::NumLiteral(
+                        token.get(1).unwrap().as_str()
+                        .parse::<i32>().expect("Should have been an integer"),
+                ));
+                continue;
+            }
+            if let Some(token) = re_string_lit.captures(source[cursor..]) {
+                cursor += token.get(0).unwrap().as_str().len();
+                tokens_vec.push(tokens::Token::StringLiteral(
+                        token.get(2).unwrap().as_str()
+                ));
+                continue;
+            }
+            if let Some(token) = re_keyword_identifier.captures(source[cursor..]) {
+                cursor += token.get(0).unwrap().as_str().len();
+                if keywords.contains(token.get(1).unwrap().as_str()) {
+                    tokens_vec.push(tokens::Token::Keyword(
+                            token.get(1).unwrap().as_str()
+                    ));
+                } else {
+                    tokens_vec.push(tokens::Token::Identifier(
+                            token.get(1).unwrap().as_str()
+                    ));
+                }
+                continue;
+            }
+            if let Some(token) = re_bool_lit.captures(source[cursor..]) {
+                cursor += token.get(1).unwrap().as_str().len();
+                match token.get(1).unwrap().as_str() {
+                    "True" => tokens_vec.push(tokens::Token::BoolLiteral(true)),
+                    "False" => tokens_vec.push(tokens::Token::BoolLiteral(false)),
+                    _ => panic!("Should have been a bool"),
+                }
+            }
         }
+        tokens_vec
     }
 }
