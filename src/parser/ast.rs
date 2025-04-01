fe3ef64250e159760c34c368874b80f625b77799
swissmichi <khrstepan@gmail.com>
2025-04-01 16:09:43 +0200

    Started work on parser

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,3 +1,50 @@
-pub struct node{
-
+pub enum Lit{
+    Int(i64),
+    Float(f64),
+    Str(String),
+    Bool(bool),
+    List(Vec<Lit>)
 }
+
+pub enum Op{
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Mod
+}
+
+pub enum Expr {
+    Lit(Lit),
+    Var(String),
+    Bin(Box<Expr>, Op, Box<Expr>),
+    Un(Op, Box<Expr>),
+}
+
+pub struct Assign {
+    pub name: String,
+    pub val: Expr,
+}
+
+pub struct FuncDef {
+    pub name: String,
+    pub args: Vec<String>,
+    pub body: Vec<Expr>,
+}
+
+pub struct FuncCall {
+    pub name: String,
+    pub args: Vec<Expr>,
+}
+
+pub struct ForLoop {
+    pub var: String,
+    pub body: Vec<Expr>,
+}
+
+pub struct WhileLoop {
+    pub cond: Box<Expr>,
+    pub body: Vec<Expr>,
+}
+
+// TODO : Finish the AST stuff
